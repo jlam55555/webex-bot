1cb2f88d214dfee33c85fdddccba1482a5536697
Glitch (blaze-capybara) <none>
2018-10-15 23:33:17 +0000

    updated bot.js

--- bot.js
+++ bot.js
@@ -17,6 +17,41 @@ This is a sample Cisco Spark bot built with Botkit.
 
 
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+// code from https://www.botkit.ai/docs/readme-ciscospark.html#getting-started
+var Botkit = require('./lib/Botkit.js');
+
+var controller = Botkit.sparkbot({
+    debug: true,
+    log: true,
+    public_address: process.env.public_address,
+    ciscospark_access_token: process.env.access_token,
+    secret: process.env.secret
+});
+
+
+var bot = controller.spawn({
+});
+
+controller.setupWebserver(process.env.PORT || 3000, function(err, webserver) {
+    controller.createWebhookEndpoints(webserver, bot, function() {
+        console.log("SPARK: Webhooks set up!");
+    });
+});
+
+controller.hears('hello', 'direct_message,direct_mention', function(bot, message) {
+    bot.reply(message, 'Hi');
+});
+
+controller.on('direct_mention', function(bot, message) {
+    bot.reply(message, 'You mentioned me and said, "' + message.text + '"');
+});
+
+controller.on('direct_message', function(bot, message) {
+    bot.reply(message, 'I got your private message. You said, "' + message.text + '"');
+});
+
+/*
 var env = require('node-env-file');
 env(__dirname + '/.env');
 
@@ -111,3 +146,4 @@ function usage_tip() {
     console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
     console.log('~~~~~~~~~~');
 }
+*/
